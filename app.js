b05496944ba3efaed1fc84163c5828cdd46b038f
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-07-23 19:22:13 -0600

    changed callback route

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.get('/auth/twitter/callback',
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect('https://powerful-tor-9925.herokuapp.com/');
   });
 
 app.use('/', routes);
